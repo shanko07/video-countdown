5ee4e7549655e2c1ba6696099696c088e2cc4a6a
Shanko <scubashanko@gmail.com>
2020-10-03 00:25:57 -0400

    ultrafast

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bash make-vid-with-countdown.sh
-ffmpeg -i 1800-countdown-timer.mp4 -i looped_audio.mp3 -shortest -af 'afade=out:st=1798:d=2' -c:v libx264 -crf 22 -preset veryfast fade_vid.mp4
+ffmpeg -i 1800-countdown-timer.mp4 -i looped_audio.mp3 -shortest -af 'afade=out:st=1798:d=2' -c:v libx264 -crf 28 -preset ultrafast fade_vid.mp4
