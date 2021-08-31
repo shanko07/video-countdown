04f5396f2be017b4e19d29fd01579611d523eb14
Shanko <scubashanko@gmail.com>
2021-08-31 15:52:08 -0400

    Configure main script to accept seconds as an argument

--- main.sh
+++ main.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # ${1} is the number of seconds
-#seconds = ${1}
-seconds=1800
+seconds=${1}
 
 bash make-vid-with-countdown.sh ${seconds}
 ffmpeg -i ${seconds}-countdown-timer.mp4 -i looped_audio.mp3 -shortest -af "afade=out:st=$(($seconds-2)):d=2" -c:v libx264 -crf 28 -preset ultrafast fade_vid.mp4
