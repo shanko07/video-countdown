4f8a7d27aa255a321852e0e753ee597a9d6fb56a
Shanko <scubashanko@gmail.com>
2022-03-17 02:11:28 -0400

    Make sure audio and video are mapped properly

--- main.sh
+++ main.sh
@@ -6,4 +6,4 @@ audio=${2}
 
 bash make-vid-with-countdown.sh ${seconds}
 echo ${audio}
-ffmpeg -i ${seconds}-countdown-timer.mp4 -i ${audio} -shortest -af "afade=out:st=$(($seconds-2)):d=2" -c:v libx264 -crf 28 -preset ultrafast fade_vid.mp4
+ffmpeg -i ${seconds}-countdown-timer.mp4 -i ${audio} -shortest -af "afade=out:st=$(($seconds-2)):d=2" -c:v libx264 -crf 28 -preset ultrafast -map 1:a:0 -map 0:v:0 fade_vid.mp4
