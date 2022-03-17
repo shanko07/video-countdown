5b2bbf953f2bb4a4b3b83216a216c31b104b9e1b
Shanko <scubashanko@gmail.com>
2022-03-17 01:03:26 -0400

    Allow for selection of audio file

--- main.sh
+++ main.sh
@@ -2,6 +2,8 @@
 
 # ${1} is the number of seconds
 seconds=${1}
+audio=${2}
 
 bash make-vid-with-countdown.sh ${seconds}
-ffmpeg -i ${seconds}-countdown-timer.mp4 -i looped_audio.mp3 -shortest -af "afade=out:st=$(($seconds-2)):d=2" -c:v libx264 -crf 28 -preset ultrafast fade_vid.mp4
+echo ${audio}
+ffmpeg -i ${seconds}-countdown-timer.mp4 -i ${audio} -shortest -af "afade=out:st=$(($seconds-2)):d=2" -c:v libx264 -crf 28 -preset ultrafast fade_vid.mp4
